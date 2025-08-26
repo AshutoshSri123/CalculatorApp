a1fd595d84b0ca7adf424b3be2875f5fa18d3ad4
AshutoshSri123 <ashu.61003@gmail.com>
2025-08-26 22:11:39 +0530

    Initial Commit

---
+++ Calculator_App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator_App
+//
+//  Created by Ashutosh Srivastava on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
